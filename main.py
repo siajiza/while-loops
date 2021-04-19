4522d6d15fd0c69d811f57575250e06b0f275ef8
Siajiza <>
2021-04-19 16:05:44 +0000

    from collections import Counter

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-# While Loops in Python
+# # While Loops in Python
 
-# On variables
-nums = list(range(1, 100))
+# # On variables
+# nums = list(range(1, 100))
 
-while len(nums) > 0:
-    print(nums.pop()) #pop() will stop the while-Look
+# while len(nums) > 0:
+#     print(nums.pop()) #pop() will stop the while-Look
 
 
-# On function
+# # On function
 
-def guessing_game():
-    while True:
-        print('What is your guess?')
-        guess = input()
+# def guessing_game():
+#     while True:
+#         print('What is your guess?')
+#         guess = input('Enter a number guess: ')
     
-        if guess == '42':
-            print('Winner!')
-            return False
-        else:
-            print(f'Try again, isn´t {guess}\n')
+#         if guess == '42':
+#             print('Winner!')
+#             return False
+#         else:
+#             print(f'Try again, isn´t {guess}\n')
 
-guessing_game()
+# guessing_game()
+
+
+from collections import Counter
+# list1 = ['x','y','z','x','x','x','y', 'z']
+# print(Counter(list1))
+
+# dog_house = ['tim', 'bob', 'gim', 'sam']
+# print(Counter(dog_house))
+
+def loop_using_while():
+    dog_house = ['tim', 'bob', 'gim', 'sam']
+    Counter = 0
+    while Counter < 10:
+        print('In our house live, ')
+        Counter += 1
+        return [dog_house, Counter]
+loop_using_while()
+
+
+
 
 
 
